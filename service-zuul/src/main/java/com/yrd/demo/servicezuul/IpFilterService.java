07d44db6f514de98d8eebfd59d47e7d197c2fd4b
wenlongliu8 <wenlongliu8@yirendai.com>
2018-07-16 11:26:30 +0800

    asdf

--- service-zuul/src/main/java/com/yrd/demo/servicezuul/IpFilterService.java
+++ service-zuul/src/main/java/com/yrd/demo/servicezuul/IpFilterService.java
@@ -1,10 +1,64 @@
 package com.yrd.demo.servicezuul;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 /**
  * Created by IDEA
  * Author:Liuwl
  * Date:2018/7/9
  * Time:16:22
  */
+@Service
 public class IpFilterService {
+
+    @Value("${demo.valid-ips}")
+    private String ips;
+
+
+    private Set<String> validIps = new HashSet<>();
+    private Set<String> validIpRegulars = new HashSet<>();
+
+    /**
+     * 判断ip是否在白名单中
+     *
+     * @param ip
+     * @return
+     */
+    public boolean isValidIp(String ip) {
+        if (validIps.contains(ip)) {
+            return true;
+        } else {
+            for (String reg : validIpRegulars) {
+                Pattern pattern = Pattern.compile(reg);
+                boolean res = pattern.matcher(ip).find();
+                if (res)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 将纯ip和带匹配的ip区分存储
+     */
+    @PostConstruct
+    private void initRegular() {
+        if (StringUtils.isBlank(ips))
+            return;
+        String[] fields = ips.split(",");
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i].contains("*")) {
+                validIpRegulars.add(fields[i]);
+            } else {
+                validIps.add(fields[i]);
+            }
+        }
+    }
 }
--- service-zuul/src/main/java/com/yrd/demo/servicezuul/MyFilter.java
+++ service-zuul/src/main/java/com/yrd/demo/servicezuul/MyFilter.java
@@ -4,6 +4,7 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 @Slf4j
 public class MyFilter extends ZuulFilter {
+    @Autowired
+    IpFilterService ipFilterService;
 
     @Override
     public String filterType() {
@@ -35,21 +38,50 @@ public class MyFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
+
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
         log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
-        Object accessToken = request.getParameter("token");
-        if(accessToken == null) {
-            log.warn("token is empty");
+        String ip = getIpAddr(request);
+        log.info("请求源IP：" + ip);
+
+
+        if (!ipFilterService.isValidIp(ip)) {
+            log.warn("非白名单ip，无法访问：" + ip);
             ctx.setSendZuulResponse(false);
-            ctx.setResponseStatusCode(401);
+            ctx.setResponseStatusCode(403);
             try {
-                ctx.getResponse().getWriter().write("token is empty");
-            }catch (Exception e){}
+                ctx.getResponse().getWriter().write("ip is invalid");
+            } catch (Exception e) {
+            }
 
             return null;
         }
+
         log.info("ok");
         return null;
     }
+
+    public String getIpAddr(HttpServletRequest request) {
+
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+
 }
